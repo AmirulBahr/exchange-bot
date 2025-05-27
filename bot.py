ab0ddae9b0cbb0c365bdd39ac31419fae8c0bf64
AmirulBahr <as0578632@gmail.com>
2025-05-27 20:56:55 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ import json
 import re
 from aiogram import Bot, Dispatcher, executor, types
 
-API_TOKEN = "7222497947:AAHcF1na-irf1FyYFJe4aOfzgtQ3PX7B8As"
+API_TOKEN = "7222497947:AAEB_KFbfutXwrW2BsavypFLDiSCu61qKY"
 ADMIN_USERNAME = "@rus_tam0"
 
 logging.basicConfig(level=logging.INFO)
